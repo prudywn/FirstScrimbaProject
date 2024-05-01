ac4c75c8400fdac41f1fd36fde00e051859eaf97
prudywn <162975278+prudywn@users.noreply.github.com>
2024-05-01 21:56:36 +0300

    First Javascript implemented file.

---
+++ index.js
@@ -0,0 +1,26 @@
+// javascript
+
+
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById('save-el')
+let count = 0
+
+function increment() {
+   count += 1
+  countEl.textContent=count
+}
+increment()
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+   let prevEntries = count + ' - ' 
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += prevEntries
+    countEl.textContent = 0
+    count = 0
+    
+    console.log(count)
+}
+
+save()
