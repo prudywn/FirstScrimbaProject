3271084b69299193b3bef369c9f6dcac6c52328c
prudywn <162975278+prudywn@users.noreply.github.com>
2024-05-02 13:55:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increment() {
    count += 1
   countEl.textContent=count
 }
-increment()
+
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
